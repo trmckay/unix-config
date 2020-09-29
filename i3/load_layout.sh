826aa2bad5aa20255eb39da0d5e556028ab55ce9
Trevor McKay <trs.mckay@gmail.com>
2020-09-29 10:00:01 -0700

    update 2020-09-29

--- i3/load_layout.sh
+++ i3/load_layout.sh
@@ -2,4 +2,4 @@
 
 i3-msg "workspace a; append_layout ~/.i3/secondary.json"
 thunderbird &
-alacritty --working-directory "/home/trevor" -e /bin/sh -c 'pfetch; checkupdates; zsh' &
+alacritty --working-directory "/home/trevor" -e /bin/sh -c 'pfetch; ~/scripts/wait-internet.sh; checkupdates; zsh' &
